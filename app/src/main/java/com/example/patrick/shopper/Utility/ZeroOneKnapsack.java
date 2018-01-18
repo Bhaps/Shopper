00af0e2e3c17501916e3a8736fbefba8f1af728a
Patrick Ma <patrickma345@gmail.com>
2018-01-18 22:52:35 +1300

    Finished coding the ZeroOneKnapsack class and added unit testing.

--- app/src/main/java/com/example/patrick/shopper/Utility/ZeroOneKnapsack.java
+++ app/src/main/java/com/example/patrick/shopper/Utility/ZeroOneKnapsack.java
@@ -1,9 +1,8 @@
 package com.example.patrick.shopper.Utility;
 
-import android.os.health.SystemHealthManager;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Executes the zero one knapsack. Takes a list of items with
@@ -17,7 +16,7 @@ public class ZeroOneKnapsack {
 
     private ArrayList<Item> items;
     private double[][] board; //Used in the algorithm
-    private Double weight; //The maximum weight that can be taken i.e. the budget in dollars
+    private Double budget; //The maximum weight that can be taken i.e. the budget in dollars
     public final static Double DEFAULT_VALUE = 1.0;
     private final Double BASIC_UNIT = 10.0; //10 cents
 
@@ -26,10 +25,10 @@ public class ZeroOneKnapsack {
 
     /**
      *
-     * @param weight The maximum weight that can be taken i.e. the budget in dollars
+     * @param budget The maximum weight that can be taken i.e. the budget in dollars
      */
-    public ZeroOneKnapsack(Double weight) {
-        this.weight = weight;
+    public ZeroOneKnapsack(Double budget) {
+        this.budget = budget;
     }
 
     /**
@@ -38,24 +37,27 @@ public class ZeroOneKnapsack {
      * @param cost The cost of the item in dollars. Will be converted into cents.
      */
     public void addItem(String info, Double cost) {
-        items.add(new Item(info, cost, DEFAULT_VALUE));
+
+        //Round the cost of the item up, if rounded down will lose some of the item's cost
+        //that may stack up to something significant and the final cost of hte
+        //maximized list will be greater than the budget
+        int costUnits = calcUnits(cost, true);
+
+        items.add(new Item(info, cost, costUnits, DEFAULT_VALUE));
     }
 
     /**
      * Set up the board where the work will be done and the items ArrayList where the items are
      * stored.
      */
-    private void initKnapsack() {
+    public void initKnapsack() {
         items = new ArrayList<Item>();
 
         //Need a base case where there are no items added. The algorithm starts at itemIndex 1
         //and capacityIndex 1. Hence first row and column are 0's to represent no items added.
         addItem("Buffer to represent no items", -1.0);
 
-        numItems = items.size();
-        maxCapacityUnits = calcUnits(weight);
 
-        board = new double[numItems + 1][maxCapacityUnits + 1];
 
         //System.out.println("Number of items: " + numItems);
         //System.out.println(Arrays.deepToString(board));
@@ -64,8 +66,16 @@ public class ZeroOneKnapsack {
     /**
      * Starts the algorithm with the items that are in the items ArrayList.
      */
-    private void start() {
-        for(int itemIndex = 1; itemIndex <= numItems; itemIndex++) {
+    private void startKnapsack() {
+        numItems = items.size();
+
+        //Round down the budget, if we round up we might end up saying we have more money than
+        //our budget actually is.
+        maxCapacityUnits = calcUnits(budget, false);
+
+        board = new double[numItems + 1][maxCapacityUnits + 1];
+
+        for(int itemIndex = 1; itemIndex < numItems; itemIndex++) {
             int capacityIndex = 1;
             while(capacityIndex <= maxCapacityUnits) {
 
@@ -74,11 +84,11 @@ public class ZeroOneKnapsack {
                 //Subtract 1 from the itemIndex to see the situation where the current item
                 //hasn't been added yet (deciding if we can add it based on a previous collection
                 //of items).
-                double ifItemAdded = get_value(itemIndex - 1,
-                        capacityIndex - calcUnits(items.get(itemIndex).getCost())) + DEFAULT_VALUE;
+                double ifItemAdded = getValue(itemIndex - 1,
+                        capacityIndex - items.get(itemIndex).getCostUnits()) + DEFAULT_VALUE;
 
                 //The value at the current capacity if we did not add the item at itemIndex.
-                double ifItemNotAdded = get_value(itemIndex - 1, capacityIndex);
+                double ifItemNotAdded = getValue(itemIndex - 1, capacityIndex);
 
                 board[itemIndex][capacityIndex] = Math.max(ifItemNotAdded, ifItemAdded);
 
@@ -89,14 +99,88 @@ public class ZeroOneKnapsack {
     }
 
     /**
+     * Returns the indexes of the items that are part of the solution to the zero one knapsack
+     * i.e. the maximized group of items that fits into the budget.
+     * @return ArrayList of the item's indexes from items.
+     */
+    private ArrayList<Integer> getSolutionIndexes() {
+        //Will contain the indexes of the items, from the array items, that are part of the solution
+        ArrayList<Integer> addedItemIndexes = new ArrayList<>();
+
+        //Start at the bottom right corner
+        int itemIndex = numItems - 1; //There is the item acting as a buffer that represents
+        //the situation of no items being added yet in the algorithm. It offets the indexes by one.
+        int capacityIndex = maxCapacityUnits;
+
+        while(itemIndex > 0 && capacityIndex > 0) {
+            if(getValue(itemIndex, capacityIndex) != getValue(itemIndex - 1, capacityIndex)) {
+                //Item was added to the knapsack
+                addedItemIndexes.add(itemIndex);
+
+                capacityIndex -= items.get(itemIndex).getCostUnits();
+            }
+            itemIndex -= 1; //Move down to the next item to see if it was added
+        }
+
+        return addedItemIndexes;
+    }
+
+    /**
+     * Take an array of indexes and return the items they correspond to.
+     * @param indexes The indexes from the elements in the ArrayList items.
+     * @return An ArrayList of the items that are the solution to the knapsack.
+     */
+    private ArrayList<Item> getSolutionItems(ArrayList<Integer> indexes) {
+        ArrayList<Item> solution = new ArrayList<>();
+
+        for(int index : indexes) {
+            solution.add(items.get(index));
+        }
+
+        return solution;
+    }
+
+    /**
+     * Returns all the information of the items that are part of the solution.
+     * @return String of all the information of the items.
+     */
+    public String solve() {
+        startKnapsack();
+
+        ArrayList<Integer> solutionIndexes = getSolutionIndexes();
+        ArrayList<Item> solutionItems = getSolutionItems(solutionIndexes);
+
+        //Retrieving the solution works backwards starting from the last item added
+        //reverse the list so it is in the same order as the items are originally added.
+        Collections.reverse(solutionItems);
+
+        String summary = "";
+        for (Item item : solutionItems) {
+            summary += item.getInformation() + "\n";
+        }
+
+        //Remove the last newline character
+        //summary.replaceAll("\n$", "");
+
+        summary = summary.trim();
+
+        return summary;
+
+    }
+
+
+
+    /**
      * Get the value for the requested item index and capacity index. If either index have gone
      * out of index, return negative infinity.
      * @param itemIndex
      * @param capacityIndex
      * @return
      */
-    private double get_value(int itemIndex, int capacityIndex) {
-        if(itemIndex > numItems || capacityIndex > maxCapacityUnits ) {
+    private double getValue(int itemIndex, int capacityIndex) {
+        //itemIndex >= numItems because there is the item that acts as a buffer to represent
+        //the case of having no items, offsets the indexes by one.
+        if(itemIndex >= numItems || capacityIndex > maxCapacityUnits ) {
             throw new java.lang.Error("The index(es) are out of bounds.");
         }
 
@@ -111,32 +195,33 @@ public class ZeroOneKnapsack {
      * Takes the weight or cost in context of the situation (e.g. dollars) and converts it into
      * the equivalent number of units.
      * @param amountDollars The amount in dollars that will be converted into units.
+     * @param roundUp Roundup if we want
      * @return The equivalent amount of units.
      */
-    public int calcUnits(Double amountDollars) {
+    public int calcUnits(Double amountDollars, boolean roundUp) {
         Double amountCents = amountDollars * 100.0;
 
-        //System.out.println("Amount in cents: " + amountCents.toString());
-        //System.out.println("Rounded: " + (Math.round(amountCents)));
-        //System.out.println("Units: " + (Math.round(amountCents) / BASIC_UNIT));
-        //System.out.println();
+        double result = Math.round(amountCents) / BASIC_UNIT;
 
-        //Will truncate the result.
-        int units = (int) (Math.round(amountCents) / BASIC_UNIT);
+        int units;
+
+        if(roundUp) {
+            units = (int) Math.round(result);
+        } else {
+            units = (int) Math.floor(result);
+        }
+
         return units;
     }
 
 
     /**
      * Used for testing
-     * @param args
-     */
+     * @param
+     */ /*
     public static void main(String[] args) {
         System.out.println("Started running");
         ZeroOneKnapsack knapsack = new ZeroOneKnapsack(1.00);
-        //System.out.println(knapsack.calcUnits(5.00));
-        //System.out.println(knapsack.calcUnits(5.54));
-        //System.out.println(knapsack.calcUnits(5.56));
 
         //knapsack.initKnapsack();
 
@@ -149,19 +234,32 @@ public class ZeroOneKnapsack {
 
         System.out.println("Started algorithm");
 
-        knapsack.start();
+        knapsack.startKnapsack();
 
 
 
         knapsack.displayBoard();
+
+        System.out.println(knapsack.getSolution());
+
         System.out.println("Done");
-    }
+    }*/
 
     public void displayBoard() {
-        System.out.println(Arrays.deepToString(board));
+        for(double[] row : board) {
+            for(double element : row) {
+                System.out.print(element);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+
+        //System.out.println(Arrays.deepToString(board));
     }
 
-
+    public ArrayList<Item> getItems() {
+        return items;
+    }
 
     /**
      * Item object used for the zero one knapsack algorithm. Used to encapsulate the needed
@@ -172,10 +270,12 @@ public class ZeroOneKnapsack {
         private String information;
         private Double cost;
         private Double value;
+        private int costUnits;
 
-        public Item(String information, Double cost, Double value) {
+        public Item(String information, Double cost, int costUnits, Double value) {
             this.information = information;
             this.cost = cost;
+            this.costUnits = costUnits;
             this.value = value;
         }
 
@@ -191,6 +291,14 @@ public class ZeroOneKnapsack {
             this.information = information;
         }
 
+        public void setCostUnits(int costUnits) {
+            this.costUnits = costUnits;
+        }
+
+        public int getCostUnits() {
+            return costUnits;
+        }
+
         public Double getValue() {
             return value;
         }
@@ -202,6 +310,30 @@ public class ZeroOneKnapsack {
         public String getInformation() {
             return information;
         }
+
+
+
+        @Override
+        public String toString() {
+            return String.format("Info: %s\nCost: %f\nCost in Units: %d\nValue: %f",
+                    this.information, this.cost, this.costUnits, this.value);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+
+            Item item = (Item) o;
+
+            if((this.cost.equals(item.cost))
+                    && (this.costUnits == item.costUnits)
+                    && (this.information == item.information)
+                    && (this.value.equals(item.value))) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
     }
 
 }
---
+++ app/src/test/java/com/example/patrick/shopper/ZeroOneKnapsackUnitTest.java
@@ -0,0 +1,131 @@
+package com.example.patrick.shopper;
+
+import com.example.patrick.shopper.Utility.ZeroOneKnapsack;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by patrick on 18/01/18.
+ */
+
+public class ZeroOneKnapsackUnitTest {
+
+    private final Double BUDGET = 5.00;
+    private ZeroOneKnapsack knapsack;
+    private double  DEFAULT_VALUE = 1.0;
+
+
+    @Before
+    public void setUp() {
+        knapsack = new ZeroOneKnapsack(BUDGET);
+        knapsack.initKnapsack();
+    }
+
+    /**
+     * Test the boundary if we get the expected result when we enter a budget of 0.
+     */
+    @Test
+    public void calcUnitsZeroTest() {
+        double amount = 0.0;
+
+        assertEquals(0, knapsack.calcUnits(amount, true));
+        assertEquals(0, knapsack.calcUnits(amount, false));
+    }
+
+    /**
+     * Test if we get the correct result if we enter an amount that does not need rounding.
+     */
+    @Test
+    public void calcUnitsNormalTest() {
+        double amount = 5.50;
+
+        assertEquals(55, knapsack.calcUnits(amount, true));
+        assertEquals(55, knapsack.calcUnits(amount, false));
+
+        double amount2 = 5.00;
+
+        assertEquals(50, knapsack.calcUnits(amount2, true));
+        assertEquals(50, knapsack.calcUnits(amount2, false));
+    }
+
+    /**
+     * Test if we get the correct result if we enter an amount that needs rounding.
+     */
+    @Test
+    public void calcUnitsRoundTest() {
+        double amount = 5.55;
+
+        assertEquals(56, knapsack.calcUnits(amount, true));
+        assertEquals(55, knapsack.calcUnits(amount, false));
+
+        double amount2 = 5.54;
+
+        assertEquals(55, knapsack.calcUnits(amount2, true));
+        assertEquals(55, knapsack.calcUnits(amount2, false));
+    }
+
+    /**
+     * Test if we are able to add items properly.
+     */
+    @Test
+    public void addItemsTest() {
+        String info1 = "A";
+        String info2 = "B";
+        String info3 = "C";
+
+        double cost1 = 1.00;
+        double cost2 = 1.10;
+        double cost3 = 1.20;
+
+        ZeroOneKnapsack.Item modelItem1 = knapsack.new Item(info1, cost1, knapsack.calcUnits(cost1, true), DEFAULT_VALUE);
+        ZeroOneKnapsack.Item modelItem2 = knapsack.new Item(info2, cost2, knapsack.calcUnits(cost2, true), DEFAULT_VALUE);
+        ZeroOneKnapsack.Item modelItem3 = knapsack.new Item(info3, cost3, knapsack.calcUnits(cost3, true), DEFAULT_VALUE);
+
+        knapsack.addItem(info1, cost1);
+        knapsack.addItem(info2, cost2);
+        knapsack.addItem(info3, cost3);
+
+        ArrayList<ZeroOneKnapsack.Item> retrievedItems = knapsack.getItems();
+
+        //At index 0 there is an Item object that is used to represent the situation of
+        //no items being added in the algorithm. Indexes > 0 are the actual items.
+        assertEquals(modelItem1, retrievedItems.get(1));
+        assertEquals(modelItem2, retrievedItems.get(2));
+        assertEquals(modelItem3, retrievedItems.get(3));
+    }
+
+    /**
+     * Test if the correct items can be retrieved.
+     */
+    @Test
+    public void solveTest() {
+        knapsack.addItem("A", 1.00);
+        knapsack.addItem("B", 1.20);
+        knapsack.addItem("C", 5.00);
+        knapsack.addItem("D", 2.10);
+        knapsack.addItem("E", 0.90);
+        knapsack.addItem("F", 0.70);
+
+        //System.out.println(knapsack.getItems());
+
+        String modelSolution = "A\nB\nD\nF";
+
+        String solution = knapsack.solve();
+
+        //knapsack.displayBoard();
+
+        //System.out.println(solution);
+
+        assertTrue(modelSolution.equals(solution));
+
+
+
+    }
+
+}
